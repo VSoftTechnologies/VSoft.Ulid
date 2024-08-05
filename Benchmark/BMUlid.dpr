d8508c56d2dc26746c2c8506cbde440bac8d8036
Vincent Parrett <vincent@finalbuilder.com>
2024-08-06 08:58:12 +1000

    Added benchmark

---
+++ Benchmark/BMUlid.dpr
@@ -0,0 +1,45 @@
+program BMUlid;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  Spring.Benchmark,
+  VSoft.Ulid;
+
+procedure BM_Create(const state: TState);
+var
+  ulid : TUlid;
+begin
+  // Perform setup here
+  for var _ in state do
+  begin
+    // This code gets timed
+    ulid := TUlid.Create;
+  end;
+end;
+
+procedure BM_Parse(const state: TState);
+var
+  ulid : TUlid;
+begin
+  // Perform setup here
+  for var _ in state do
+  begin
+    // This code gets timed
+    ulid := TUlid.Parse('01J4JC3MY6S461H3P96FB4QAGS');
+  end;
+end;
+
+
+begin
+
+  // Register the function as a benchmark
+  Benchmark(BM_Create, 'TUlid.Create');
+  Benchmark(BM_Parse, 'TUlid.Parse');
+  // Run the benchmark
+  Benchmark_Main;
+  Readln;
+end.
